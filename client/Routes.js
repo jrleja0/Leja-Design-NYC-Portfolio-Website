2bde68b74ed2c2845a225662deeb3c15690f3a2d
Jasiu Leja <jleja09@gmail.com>
2017-07-31 21:33:46 -0400

    feature: adds social media icon bar and styling to Contact component

--- client/Routes.js
+++ client/Routes.js
@@ -1,10 +1,10 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {Router} from 'react-router';
-import {Route, Switch} from 'react-router-dom';
+import {Route, Switch, IndexRedirect} from 'react-router-dom';
 import PropTypes from 'prop-types';
 import history from './history';
-import {Contact, HomeBody, Main} from './components';
+import {Contact, HomeBody, Main, Resume} from './components';
 
 /*///
  COMPONENT
--- client/components/Contact.js
+++ client/components/Contact.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
+import {Link} from 'react-router-dom';
+
 
 /*///
  COMPONENT
@@ -10,18 +12,45 @@ const Contact = (props) => {
   return (
     <div>
       <div className="container-fluid redBackground">
-        <div>
+        <div className="div-contactMeTitle">
           <img className="img-fluid" src="/assets/text/contactMeAndConnectBlue.png" alt="Contact Me And Connect" />
         </div>
         <div className="container-fluid">
           <div className="row top-bottom-margin-30">
             <div className="headshotBackground col-lg-6 col-md-6 col-sm-12" />
             <div className="contact-textbox col-lg-6 col-md-6 col-sm-12">
-              <ul>
-                <li>Email</li>
-                <li>Resume</li>
-                <li>Links (GitHub, LinkedIn, & Instagram)</li>
-              </ul>
+              <div className="contact-textbox-inner">
+                <ul>
+                  <li className="li-extra-padding-bottom">
+                    <Link className="glyphicon glyphicon-envelope" to="mailto:lejadesign@gmail.com">
+                      <span> lejadesign@gmail.com</span>
+                    </Link>
+                  </li>
+                  <li className="glyphicon glyphicon-briefcase li-extra-padding-bottom"><span> Resume</span></li>
+                  <br />
+                  <li className="glyphicon glyphicon-link"><span> Links:</span></li>
+                  <li><span className="social-labels"> GitHub  |  LinkedIn  |  Instagram</span></li>
+                  <li>
+                    <div className="div-socialMediaLinks">
+                      <span>
+                        <Link to="https://github.com/jrleja0">
+                        <img className="social-icon" src="/assets/social_icons/github.png" alt="GitHub profile" />
+                        </Link>
+                      </span>
+                      <span>
+                        <Link to="https://www.linkedin.com/in/jrleja">
+                        <img className="social-icon" src="/assets/social_icons/linkedin.png" alt="LinkedIn profile" />
+                        </Link>
+                      </span>
+                      <span>
+                        <Link to="https://www.instagram.com/jrleja">
+                        <img className="social-icon" src="/assets/social_icons/instagram.png" alt="Instagram profile" />
+                        </Link>
+                      </span>
+                    </div>
+                  </li>
+                </ul>
+              </div>
             </div>
           </div>
         </div>
