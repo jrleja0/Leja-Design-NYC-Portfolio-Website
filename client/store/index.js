3495af4871432d730b73d6ee200482337b126ca1
Jasiu Leja <jleja09@gmail.com>
2017-08-21 12:06:14 -0400

    fixes contact email link--removes underline when clicked--and removes redux-logger

--- client/store/index.js
+++ client/store/index.js
@@ -1,10 +1,9 @@
 import {createStore, combineReducers, applyMiddleware} from 'redux';
-import createLogger from 'redux-logger';
 import thunkMiddleware from 'redux-thunk';
 import imageStore from './imageStore';
 
 const reducer = combineReducers({ imageStore });
-const middleware = applyMiddleware(thunkMiddleware, createLogger({collapsed: true}));
+const middleware = applyMiddleware(thunkMiddleware);
 const store = createStore(reducer, middleware);
 
 export default store;
