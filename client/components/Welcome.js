2b82ed46b3711c7794b3cf8201b490c0cc31cde1
Jasiu Leja <jleja09@gmail.com>
2017-10-01 17:52:30 -0400

    changes layout of Project images and textboxes, updates Project textbox colors, and changes background layout of Welcome

--- client/components/Welcome.js
+++ client/components/Welcome.js
@@ -12,6 +12,8 @@ const Welcome = (props) => {
           <img className="img-fluid" src="/assets/abstractions/welcome_image.jpg" alt="Welcome" />
           <div className="div-img-cover" />
         </div>
+      </div>
+      <div className="container-fluid black-with-blue">
         <div className="welcome-textbox">
           <ul>
             <li><span className="green-text">J R Leja Design NYC</span> showcases the web apps, games, and other</li>
