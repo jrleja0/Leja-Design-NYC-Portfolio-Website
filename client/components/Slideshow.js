20662299fba63fed668eb2b0ba5658b8830316ec
Jasiu Leja <jleja09@gmail.com>
2017-11-28 12:11:53 -0500

    removes commented out react-bootstrap carousel

--- client/components/Slideshow.js
+++ client/components/Slideshow.js
@@ -1,5 +1,4 @@
 import React from 'react';
-// import {Carousel} from 'react-bootstrap';
 
 /*///
  COMPONENT
@@ -87,28 +86,6 @@ class Slideshow extends React.Component {
           </div>
         </div>
       </div>
-
-      // <div>
-      //   <Carousel>
-      //     <Carousel.Item>
-      //       <img width={900} height={500} alt="Welcome!" src="/assets/welcomeSlideWide.jpg" />
-      //       <Carousel.Caption />
-      //     </Carousel.Item>
-      //     <Carousel.Item>
-      //       <img width={900} height={500} alt="Software Projects" src="/assets/projectSlideWide.jpg" />
-      //       <Carousel.Caption>
-      //         <h3>Software Projects</h3>
-      //       </Carousel.Caption>
-      //     </Carousel.Item>
-      //     <Carousel.Item>
-      //       <img width={900} height={500} alt="Art" src="/assets/artSlideWide.jpg" />
-      //       <Carousel.Caption>
-      //         <h3>Art</h3>
-      //       </Carousel.Caption>
-      //     </Carousel.Item>
-      //   </Carousel>
-      // </div>
-
     );
   }
 }
