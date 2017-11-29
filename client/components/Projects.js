700d35ca6cbdc26d9e533054b80c1f5e0cffd45f
Jasiu Leja <jleja09@gmail.com>
2017-11-29 17:34:20 -0500

    feature: when clicking scroll to top button, adds opacity transition (fade out/in) to body; also adds scss to fix background: fixed / background: cover bug in mobile iOS - need to test

--- client/components/Projects.js
+++ client/components/Projects.js
@@ -11,124 +11,126 @@ const Projects = (props) => {
 
   return (
     <div>
-      <div className="container-fluid greenBackground">
-        <div className="row">
-          <div className="div-project-main-title">
-            <h1 className="heading-font">Recent Projects<br />
-              <span className="subheading-font">Apps | Games | Tech Talks</span>
-            </h1>
+      <div className="greenBackground">
+        <div className="container-fluid">
+          <div className="row">
+            <div className="div-project-main-title">
+              <h1 className="heading-font">Recent Projects<br />
+                <span className="subheading-font">Apps | Games | Tech Talks</span>
+              </h1>
+            </div>
           </div>
-        </div>
-        <div className="projects-main-container container-fluid">
-          {
-          imageData && imageData.length ? imageData.map(image => (
-            <div key={image.name}>
-              <div className="div-project-title">
-                <img src={ image.titlePath } alt={ image.projectName } />
-                <div className="div-img-cover" />
-              </div>
-              <div className="row">
-                <div
-                  className={ `img-main-project ${image.name}` }
-                  style={ { backgroundImage: `url( ${image.imagePath} )` } }
-                />
-              </div>
-              <div className="row project-text-row">
-                <div className="project-textbox">
-                  <div className="col-lg-7 col-md-7 col-sm-12">
-                    <ul className="description-column">
-                      <li>
-                        { image.description }
-                      </li>
-                      <li>
-                        {
-                          image.technologies ?
-                            <span>Technologies used: <br />
-                            { image.technologies.split(';').map((tech, i) => (
-                              i === ( image.technologies.split(';').length - 1 ) ?
-                              <span key={tech}><span>{ tech }</span><br /></span>
-                              : <span key={tech}><span>{ tech };</span><br /></span>
-                              ))
-                            }
-                            </span> : null
-                        }
-                      </li>
-                    </ul>
-                  </div>
-                  <div className="col-lg-5 col-md-5 col-sm-12">
-                    <ul className="links-column">
-                      <li className="links-title li-add-padding-bottom-10">
-                        <i className="fa fa-link" aria-hidden="true" />
-                        <span>&ensp; </span><span> Links:</span>
-                      </li>
-                      <li>
-                        <div className="basic-div-links">
-                          <table>
-                            <tbody>
-                              {
-                                image.linkProject ?
-                                  <tr>
-                                    <td>
-                                      <p>Check It Out:</p>
-                                    </td>
-                                    <td>
-                                      <a className="a-td-button" href={image.linkProject} type="button" aria-label="Project Link">
-                                        <i className="fa fa-link link-icon" aria-hidden="true" />
-                                      </a>
-                                    </td>
-                                  </tr>
-                                  : null
+          <div className="projects-main-container container-fluid">
+            {
+            imageData && imageData.length ? imageData.map(image => (
+              <div key={image.name}>
+                <div className="div-project-title">
+                  <img src={ image.titlePath } alt={ image.projectName } />
+                  <div className="div-img-cover" />
+                </div>
+                <div className="row">
+                  <div
+                    className={ `img-main-project ${image.name}` }
+                    style={ { backgroundImage: `url( ${image.imagePath} )` } }
+                  />
+                </div>
+                <div className="row project-text-row">
+                  <div className="project-textbox">
+                    <div className="col-lg-7 col-md-7 col-sm-12">
+                      <ul className="description-column">
+                        <li>
+                          { image.description }
+                        </li>
+                        <li>
+                          {
+                            image.technologies ?
+                              <span>Technologies used: <br />
+                              { image.technologies.split(';').map((tech, i) => (
+                                i === ( image.technologies.split(';').length - 1 ) ?
+                                <span key={tech}><span>{ tech }</span><br /></span>
+                                : <span key={tech}><span>{ tech };</span><br /></span>
+                                ))
                               }
-                              {
-                                image.linkDemo ?
-                                  <tr>
-                                    <td>
-                                      <p>
-                                        { image.name === 'techTalkPaperJS1' ?
-                                          <span>Watch <br />Presentation:</span> : <span>Watch Demo <br />Presentation:</span>
-                                        }
-                                      </p>
-                                    </td>
-                                    <td>
-                                      <a className="a-td-button" href={image.linkDemo} type="button" aria-label="YouTube Presentation Link">
-                                        <i className="fa fa-youtube-play social-icon" aria-hidden="true" />
-                                      </a>
-                                    </td>
-                                  </tr>
-                                  : null
-                              }
-                              {
-                                image.linkGitHub ?
-                                  <tr>
-                                    <td>
-                                      <p>See GitHub <br />Repository:</p>
-                                    </td>
-                                    <td>
-                                      <a className="a-td-button" href={image.linkGitHub} type="button" aria-label="GitHub Project Link">
-                                        <i className="fa fa-github social-icon" aria-hidden="true" />
-                                      </a>
-                                    </td>
-                                  </tr>
-                                  : null
-                              }
-                            </tbody>
-                          </table>
-                        </div>
-                      </li>
-                    </ul>
+                              </span> : null
+                          }
+                        </li>
+                      </ul>
+                    </div>
+                    <div className="col-lg-5 col-md-5 col-sm-12">
+                      <ul className="links-column">
+                        <li className="links-title li-add-padding-bottom-10">
+                          <i className="fa fa-link" aria-hidden="true" />
+                          <span>&ensp; </span><span> Links:</span>
+                        </li>
+                        <li>
+                          <div className="basic-div-links">
+                            <table>
+                              <tbody>
+                                {
+                                  image.linkProject ?
+                                    <tr>
+                                      <td>
+                                        <p>Check It Out:</p>
+                                      </td>
+                                      <td>
+                                        <a className="a-td-button" href={image.linkProject} type="button" aria-label="Project Link">
+                                          <i className="fa fa-link link-icon" aria-hidden="true" />
+                                        </a>
+                                      </td>
+                                    </tr>
+                                    : null
+                                }
+                                {
+                                  image.linkDemo ?
+                                    <tr>
+                                      <td>
+                                        <p>
+                                          { image.name === 'techTalkPaperJS1' ?
+                                            <span>Watch <br />Presentation:</span> : <span>Watch Demo <br />Presentation:</span>
+                                          }
+                                        </p>
+                                      </td>
+                                      <td>
+                                        <a className="a-td-button" href={image.linkDemo} type="button" aria-label="YouTube Presentation Link">
+                                          <i className="fa fa-youtube-play social-icon" aria-hidden="true" />
+                                        </a>
+                                      </td>
+                                    </tr>
+                                    : null
+                                }
+                                {
+                                  image.linkGitHub ?
+                                    <tr>
+                                      <td>
+                                        <p>See GitHub <br />Repository:</p>
+                                      </td>
+                                      <td>
+                                        <a className="a-td-button" href={image.linkGitHub} type="button" aria-label="GitHub Project Link">
+                                          <i className="fa fa-github social-icon" aria-hidden="true" />
+                                        </a>
+                                      </td>
+                                    </tr>
+                                    : null
+                                }
+                              </tbody>
+                            </table>
+                          </div>
+                        </li>
+                      </ul>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-          ))
-          : <div className="div-container-image-under-construction">
-              <img
-                src="/assets/under_construction.gif"
-                alt="Sorry, page is under construction."
-              />
-              <div className="div-img-cover" />
-            </div>
-          }
+            ))
+            : <div className="div-container-image-under-construction">
+                <img
+                  src="/assets/under_construction.gif"
+                  alt="Sorry, page is under construction."
+                />
+                <div className="div-img-cover" />
+              </div>
+            }
+          </div>
         </div>
       </div>
     </div>
