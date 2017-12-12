79185db66372473aeef00c0cc7a5a6f70f523b0c
Jasiu Leja <jleja09@gmail.com>
2017-12-12 17:33:46 -0500

    experiments with adding Transition and CSSTransition components; refactors React-Router Routes to work with the transitions

--- client/index.js
+++ client/index.js
@@ -4,10 +4,14 @@ import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
 import store from './store';
 import Routes from './routes';
+import {Router} from 'react-router';
+import history from './history';
 
 ReactDOM.render(
   <Provider store={store}>
-    <Routes />
+    <Router history={history}>
+      <Routes />
+    </Router>
   </Provider>,
   document.getElementById('app')
 );
