9aa51bbbe0927c574a0eee0d59826496864a9523
Jasiu Leja <jleja09@gmail.com>
2017-12-13 12:59:29 -0500

    tweaks CSSTransition scss

--- client/Routes.js
+++ client/Routes.js
@@ -28,52 +28,24 @@ class Routes extends Component {
     this.props.loadInitialData();
   }
 
-  onTransEnter() {
-    console.log('hit enter');
-    const body = document.getElementsByTagName('body')[0];
-    body.style.opacity = 1;
-  }
-
-  onTransExit() {
-    console.log('hit exit');
-    const body = document.getElementsByTagName('body')[0];
-    body.style.opacity = 0;
-  }
-
   render () {
     console.log('loc', location.pathname.split('/')[1] || '/', this.props.location.pathname.split('/')[1]);
 
     return (
       // <Router history={history}>
         <TransitionGroup>
-          <Transition timeout={3000} mountOnEnter={true} unmountOnExit={true} appear
-            onEnter={this.onTransEnter} onExit={this.onTransExit}>
-            {/* {(status => {
-              const body = document.getElementsByTagName('body')[0];
-              switch (status) {
-                case 'entering':
-                  body.style.opacity = 1;
-                  break;
-                case 'exiting':
-                  body.style.opacity = 0;
-              }
-              console.log('status', status); */}
-          {/* <CSSTransition key={this.props.location.pathname.split('/')[1] || '/'} timeout={3000} appear
-            classNames="fading-transition" mountOnEnter={true} unmountOnExit={true}> */}
-              {/* return ( */}
-                <Main>
-                  <Switch location={this.props.location}>
-                    <Route path="/home" component={Home} />
-                    <Route path="/projects" component={Projects} />
-                    <Route path="/art" component={ArtShowcase} />
-                    <Route path="/contact" component={Contact} />
-                    <Redirect to="/home" />
-                  </Switch>
-                </Main>
-              {/* ); */}
-          {/* </CSSTransition> */}
-            {/* })} */}
-          </Transition>
+          <CSSTransition key={this.props.location.pathname.split('/')[1] || '/'} timeout={3000} appear
+            classNames="fading-transition" mountOnEnter={true} unmountOnExit={true}>
+            <Main>
+              <Switch location={this.props.location}>
+                <Route path="/home" component={Home} />
+                <Route path="/projects" component={Projects} />
+                <Route path="/art" component={ArtShowcase} />
+                <Route path="/contact" component={Contact} />
+                <Redirect to="/home" />
+              </Switch>
+            </Main>
+          </CSSTransition>
         </TransitionGroup>
       // </Router>
     );
