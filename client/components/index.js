e9f6928d20f7f0526bf4410575b94549dcdca959
Jasiu Leja <jleja09@gmail.com>
2017-12-30 15:51:23 -0500

    removes ScrollToTop component, which is not being used

--- client/components/index.js
+++ client/components/index.js
@@ -3,7 +3,6 @@ export {default as MainLogo} from './MainLogo';
 export {default as ArtShowcase} from './ArtShowcase';
 export {default as Contact} from './Contact';
 export {default as Projects} from './Projects';
-export {default as ScrollToTop} from './ScrollToTop';
 export {default as Slideshow} from './Slideshow';
 export {default as Spinner} from './Spinner';
 export {default as Welcome} from './Welcome';
