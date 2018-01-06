96026355857974a6dcc39965045e2b705de73145
Jasiu Leja <jleja09@gmail.com>
2018-01-06 16:29:12 -0500

    updates seed file and Welcome component content

--- client/components/Welcome.js
+++ client/components/Welcome.js
@@ -12,7 +12,7 @@ const Welcome = (props) => {
           <ul>
             <li><span className="green-text">J R Leja Design NYC</span> showcases the web apps, games, and other</li>
             <li className="li-add-padding-bottom-20">software development and art projects of <span className="green-text">Jasiu Leja</span>.</li>
-            <li>Jasiu is a NYC-based Fullstack/Front-End <span className="green-text">Software Developer</span></li>
+            <li>Jasiu is a NYC-based Full Stack | Front End <span className="green-text">Software Developer</span></li>
             <li className="li-add-padding-bottom-20">with a passion for art and design.</li>
             <li>Thanks for visiting!</li>
           </ul>
