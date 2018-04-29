2a977d2fa472b7cd6c54b270d8d6fd43e92064f3
Jasiu Leja <jleja09@gmail.com>
2018-04-29 16:37:43 -0400

    updates readme and seed file content; deletes old seed file

--- bin/seed.js
+++ bin/seed.js
@@ -9,7 +9,7 @@ const data = {
       category: 'project',
       projectName: 'SecondDibs//Ecommerce Site',
       description: 'Ecommerce site inspired by 1stdibs.com — Users can browse and search items, make purchases, view order history, and save a list of favorite items, with or without creating a user account',
-      technologies: 'JavaScript - React, Redux; Node, Express, PostgreSQL/Sequelize, Express-Session & Passport.js for authentication; HTML/SCSS, Material-UI',
+      technologies: 'JavaScript - React, Redux; Node, Express, PostgreSQL | Sequelize, Express-Session & Passport.js for authentication; HTML, SCSS, Material-UI',
       linkProject: 'https://seconddibs.herokuapp.com',
       linkGitHub: 'https://github.com/jrleja0/SecondDibs-Ecommerce-Site',
     },
@@ -19,7 +19,7 @@ const data = {
       category: 'project',
       projectName: 'Smash-Bots',
       description: 'Smash-Bots is a 2-D multiplayer online fighting game, loosely based off the game Super Smash Brothers, with an item store and robots. This group project was completed in two and a half weeks.',
-      technologies: 'JavaScript - Phaser, React, and Redux for the front end; Node, Express, and PostgreSQL/Sequelize for the back end; Socket.io for multiplayer functionality; HTML/CSS.',
+      technologies: 'JavaScript - Phaser, React, and Redux for the front end; Node, Express, and PostgreSQL | Sequelize for the back end; Socket.io for multiplayer functionality; HTML, CSS',
       linkProject: 'https://smashbots.herokuapp.com',
       linkDemo: 'https://www.youtube.com/watch?v=903YYFBc9V8',
       linkGitHub: 'https://github.com/jrleja0/Smash-Bots',
@@ -30,32 +30,10 @@ const data = {
       category: 'project',
       projectName: 'D3.js Visualization://Top Searched Visual Artists',
       description: 'Data visualizations using D3.js of the top 20 most searched visual artists on artnet.com in Sept 2017. Visualizations include: tree map, bubble chart, and plain list.',
-      technologies: 'JavaScript - React, D3; Node, Express; HTML/SCSS',
+      technologies: 'JavaScript - React, D3; Node, Express; HTML, SCSS',
       linkProject: 'https://top-searched-artists-09-2017.herokuapp.com',
       linkGitHub: 'https://github.com/jrleja0/D3-Visualization-Top-Searched-Artists',
     },
-    /*
-    {
-      name: 'easyPressReleaseMaker1',
-      imagePath: 'assets/projects/easy_press_release_maker/screenshot1.jpg',
-      category: 'project',
-      projectName: 'Easy Press Release//Maker',
-      description: 'A prototype full stack RESTful CRUD app that creates press releases quickly and easily. This solo project is in development.',
-      technologies: 'JavaScript - React, Redux; Node, Express, and PostgreSQL/Sequelize; HTML/SCSS.',
-      linkGitHub: 'https://github.com/jrleja0/Easy-Press-Release-Maker',
-    },
-    */
-    /*
-    {
-      name: 'quickLookChromeExt1',
-      imagePath: 'assets/projects/quick_look_chrome_ext/screenshot1.jpg',
-      category: 'project',
-      projectName: '"Quick Look" Google Search//Chrome Extension',
-      description: 'A simple Chrome extension that searches Google for top titles related to your search.',
-      technologies: 'JavaScript - React, Axios, Cheerio, Webpack; HTML/CSS',
-      linkGitHub: 'https://github.com/jrleja0/quick-look-google-search-chrome-extension',
-    },
-    */
   ]
 };
 
